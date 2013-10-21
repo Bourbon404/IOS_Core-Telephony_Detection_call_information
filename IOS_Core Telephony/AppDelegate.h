463aec92f657c514b61d8a314f763ac450f8042a
Bourbon <bourbon@jerrytekiMac.local>
2013-10-21 14:15:02 +0800

    Initial Commit

---
+++ IOS_Core Telephony/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IOS_Core Telephony
+//
+//  Created by Bourbon on 13-10-21.
+//  Copyright (c) 2013年 mamarow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IOS_Core Telephony/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IOS_Core Telephony
+//
+//  Created by Bourbon on 13-10-21.
+//  Copyright (c) 2013年 mamarow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
